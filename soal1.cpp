67616340e184bd7fa3d51b9519cc65f7d1fbff66
Nadhyfad <nadhyfadina@gmail.com>
2025-10-01 21:17:37 +0700

    Add files via upload

---
+++ soal1.cpp
@@ -0,0 +1,25 @@
+// 103112430052_DINA NADHYFA
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int angka1, angka2;
+    cout << "masukkan angka1 : ";
+    cin >> angka1;
+    cout << "masukkan angka2 : ";
+    cin >> angka2;
+
+    //penjumlahan
+    cout << "penjumlahan : " << angka1 + angka2 << endl;
+    //pengurangan
+    cout << "pengurangan : " << angka1 - angka2 << endl;
+    //perkalian
+    cout << "perkalian : " << angka1 * angka2 << endl;
+    //pembagian
+    cout << "pembagian : " << angka1 / angka2 << endl;
+    //modulus
+    cout << "modulus : " << angka1 % angka2 << endl;
+    
+    return 0;
+}
